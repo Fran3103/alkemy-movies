5ede79d0a443f58dfb307a37bacde67ad8d8dec5
Franco Aguirre <fn.aguirre@hotmail.com>
2023-09-21 00:07:58 -0300

    arreglo de links

--- src/Components/Paginas/Footer.jsx
+++ src/Components/Paginas/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return (
     <footer className=''>
 
-        <nav className='max-w-full absolut bottom-0 flex justify-center items-center gap-12 mt-20'>
+        <nav className='max-w-full absolut bottom-0 flex justify-center items-center gap-12 mt-20 mb-12'>
             <ul>
                 <li>
-                    <a href="/" rel='nooopenei noreferrer'>Github</a>
+                    <a href="https://github.com/Fran3103/alkemy-movies.git" target='_Blank' rel='nooopenei noreferrer'>Github</a>
                 </li>
             </ul>
             <ul>
                 <li>
-                    <a href="/"  rel='nooopenei noreferrer'>Linkeind</a>
+                    <a href="https://www.linkedin.com/in/franco-aguirre-24855012a"  target='_blank' rel='nooopenei noreferrer'>Linkednd</a>
                 </li>
             </ul>
             
